e510600b5a02d7b263a100f168f62929be87a003
chukwukaoranile <prosperbinary@gmail.com>
2023-04-21 04:59:51 -0700

    configuration

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-#a file in /tmp.
+# a file in /tmp.
 file { '/tmp/school':
-content => 'I love Puppet',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
+  ensure => file,
+  content => 'I love Puppet',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
 }
