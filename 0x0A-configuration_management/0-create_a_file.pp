9a82f3f3c6a166eec7a8f9055d113e70b4bbeb75
chukwukaoranile <prosperbinary@gmail.com>
2023-04-21 05:02:20 -0700

    configuration

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# a file in /tmp.
+# Using pippet, creates a file in the /tmp directory
 file { '/tmp/school':
-  ensure => file,
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
 }
+
