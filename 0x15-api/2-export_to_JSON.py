d288c049a8346aab4f8fbfe60eaa8b732c17a445
chukwukaoranile <prosperbinary@gmail.com>
2023-05-23 07:44:25 +0100

    API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,7 +6,7 @@ Requirements:
 Records all tasks that are owned by this employee
 Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed":
 TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE",
-	"completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+"completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
 File name must be: USER_ID.json
 """
 import json
