ed071246439c0d3e224d0e9374f51343ef654945
chukwukaoranile <prosperbinary@gmail.com>
2023-05-23 07:41:57 +0100

    API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,7 +4,9 @@ A extend your Python script to export data in the JSON format,
 Using what you did in the task #0.
 Requirements:
 Records all tasks that are owned by this employee
-Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE",
+	"completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
 File name must be: USER_ID.json
 """
 import json
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """
-A Python script to export data in the JSON format, Using what you did in the task #0
+A Python script to export data in the JSON format,
+  Using what you did in the task #0
 Requirements:
 Records all tasks from all employees
-Format must be: { "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ], "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ]}
+Format must be: { "USER_ID": [ {"username": "USERNAME", "task":
+"TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME",
+"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ], "USER_ID":
+[ {"username": "USERNAME", "task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS}, {"username": "USERNAME", "task": "TASK_TITLE",
+"completed": TASK_COMPLETED_STATUS}, ... ]}
 File name must be: todo_all_employees.json
 """
 
